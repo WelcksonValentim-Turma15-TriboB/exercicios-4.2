cbb090ca21031b1e520183c0f6881af63432ddce
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-07-29 19:54:17 -0300

    Inclui exemplos, referente ao bloco 4, segundo dia.

--- exemplo.js
+++ exemplo.js
@@ -1,4 +1,23 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = ;
+//Exemplos sobre Arrays//
+//Obtenha o valor "Serviços" do array menu//
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = menu[1];
+//console.log(menuServices);
 
-console.log(menuServices);
+//Procure o índice do valor "Portfólio" do array menu //
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let indexOfPortfolio = menu.indexOf('Portfólio');
+//console.log(indexOfPortfolio);
+
+//Adicione o valor "Contato" no final do array menu  //
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//menu.push("Contato")
+//console.log(menu);
+
+
+//Exemplos sobre For//
+//Utilize o for para imprimir os elementos da lista groceryList com o console.log()//
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index < groceryList.length; index += 1){
+console.log(groceryList[index])
+}
