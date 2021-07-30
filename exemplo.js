8f204fe49fa6df85aa54e7d4158a08bb6ade26b4
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-07-30 10:39:43 -0300

    Adiciona exemplo dos comandos for/of

--- exemplo.js
+++ exemplo.js
@@ -17,7 +17,13 @@
 
 //Exemplos sobre For//
 //Utilize o for para imprimir os elementos da lista groceryList com o console.log()//
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
-for (let index = 0; index < groceryList.length; index += 1){
-console.log(groceryList[index])
+//let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+//for (let index = 0; index < groceryList.length; index += 1){
+//console.log(groceryList[index])
+//}
+
+//Exemplos sobre For/of//
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let name of names){
+console.log(name);
 }
